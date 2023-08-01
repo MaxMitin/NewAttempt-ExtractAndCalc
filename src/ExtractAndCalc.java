f080baaf337fcf296bb8dc47700ba95082699da3
MaxMitin <maksim.mitin@mail.ru>
2023-08-01 17:23:14 +0300

    Тренировка.

--- src/ExtractAndCalc.java
+++ src/ExtractAndCalc.java
@@ -1,17 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ExtractAndCalc {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String TextFile = "a1.txt";
+        try {
+            Scanner scannerFile = new Scanner(new File(TextFile));
+            int x = scannerFile.nextInt();
+            int y = scannerFile.nextInt();
+            System.out.println("Сумма извлеченых чисел равна значению: " + (x+y));
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Ой-ой!");
         }
     }
 }
+
+
+
+
+
+
+
+
